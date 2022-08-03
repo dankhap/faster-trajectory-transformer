4eab27e13031809c50e658db52634aedad68fdf1
dankhap <51119602+dankhap@users.noreply.github.com>
2022-08-03 14:59:08 +0300

    simplified sample

--- trajectory/planning/sample.py
+++ trajectory/planning/sample.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 
 from trajectory.utils.common import top_k_logits, round_to_multiple
 
-
+# @profile
 def _sample_inner(logits, top_k, temperature, greedy=False):
     logits = logits / temperature
 
@@ -21,7 +21,10 @@ def _sample_inner(logits, top_k, temperature, greedy=False):
 
     return idx
 
-@profile
+def _sample_inner_deterministic(logits, top_k, temperature, greedy=False):
+    return torch.topk(logits, k=1, dim=-1)[-1]
+
+# @profile
 def sample(model, context, steps, top_k=None, model_state=None, temperature=1.0, greedy=True):
     batch_size = context.shape[0]
 
@@ -49,7 +52,8 @@ def sample(model, context, steps, top_k=None, model_state=None, temperature=1.0,
 
         logits, model_state = model(context[:, -1:], state=model_state)
 
-        sampled_tokens = _sample_inner(logits[:, -1, :], top_k, temperature, greedy)
+        # sampled_tokens = _sample_inner(logits[:, -1, :], top_k, temperature, greedy)
+        sampled_tokens = _sample_inner_deterministic(logits[:, -1, :], top_k, temperature, greedy)
         context = torch.hstack([context, sampled_tokens])
 
         raw_logits[:, t] = logits[:, -1, :]
